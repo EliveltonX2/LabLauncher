3089cb5a334c9b197418f7d48177f0c550e97cec
EliveltonX2 <elivelton.pardini@gmail.com>
2025-06-19 01:58:40 -0400

    Registra modelos no painel de administração

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin
+# catalog/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Category, Part, Project
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Configuração de admin para o modelo Category.
+    """
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)} # Preenche o slug automaticamente a partir do nome
+
+@admin.register(Part)
+class PartAdmin(admin.ModelAdmin):
+    """
+    Configuração de admin para o modelo Part.
+    """
+    list_display = ('name', 'author', 'category', 'status', 'created_at')
+    list_filter = ('status', 'category', 'author')
+    search_fields = ('name', 'description', 'author__username')
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    """
+    Configuração de admin para o modelo Project.
+    """
+    list_display = ('title', 'author', 'category', 'status', 'created_at')
+    list_filter = ('status', 'category', 'author')
+    search_fields = ('title', 'description', 'author__username')
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,25 @@
-from django.contrib import admin
+# users/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    """
+    Define a configuração de admin para o nosso CustomUser.
+    Vamos adicionar nossos campos customizados ('is_approved', 'profile_picture')
+    para que apareçam no painel.
+    """
+    # Adiciona os campos ao formulário de criação de usuário
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('is_approved', 'profile_picture')}),
+    )
+    # Adiciona os campos ao formulário de edição de usuário
+    fieldsets = UserAdmin.fieldsets + (
+        ('Status de Aprovação', {'fields': ('is_approved', 'profile_picture')}),
+    )
+    # Mostra os campos na lista principal de usuários
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'is_approved']
+
+# Registra o nosso modelo CustomUser com a nossa configuração customizada
+admin.site.register(CustomUser, CustomUserAdmin)
