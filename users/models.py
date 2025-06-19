fecf4b92cd52c63ea4b7d9e7b1fa79366ec6b545
EliveltonX2 <elivelton.pardini@gmail.com>
2025-06-19 00:05:32 -0400

    Resolve conflito do CustomUser e adiciona .gitkeep

--- users/models.py
+++ users/models.py
@@ -1,13 +1,9 @@
+# users/models.py (CÓDIGO CORRIGIDO)
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-
-# Create your models here.
 class CustomUser(AbstractUser):
-    """
-    Modelo de usuário customizado que herda do padrão do Django.
-    Adicionamos campos para foto de perfil e status de aprovação.
-    """
+    # Nossos campos customizados
     profile_picture = models.ImageField(
         upload_to='profile_pics/',
         null=True,
@@ -20,6 +16,23 @@ class CustomUser(AbstractUser):
         help_text='Indica se o usuário foi aprovado para acessar o conteúdo.'
     )
 
+    # Campos para resolver o conflito
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_name="customuser_set", # Nome único para o acesso reverso
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name="customuser_set", # Nome único para o acesso reverso
+        related_query_name="user",
+    )
 
-def __str__(self):
+    def __str__(self):
         return self.username
