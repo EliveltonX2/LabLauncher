35f66bf307d8412bfbdbeab1dc1526c50c14df2e
EliveltonX2 <elivelton.pardini@gmail.com>
2025-06-22 01:57:51 -0400

    Adiciona migracao para dropar tabelas do catalogo2

--- build.sh
+++ build.sh
@@ -2,23 +2,7 @@
 # exit on error
 set -o errexit
 
-echo "--- INICIANDO BUILD SCRIPT COM SINCRONIZACAO DE BANCO ---"
-
 pip install -r requirements.txt
 python manage.py collectstatic --no-input --clear
-
-# --- A SINCRONIZAÇÃO ---
-
-# Passo 1: Aplica todas as migrações de TODOS OS OUTROS apps primeiro.
-echo "--- Aplicando migracoes de outros apps (auth, admin, etc.) ---"
-python manage.py migrate --exclude catalog
-
-# Passo 2: Agora, força o alinhamento do histórico do app 'catalog'.
-# Ele vai marcar 0001_initial como aplicada sem tentar criar as tabelas que já existem.
-echo "--- Rodando --fake-initial especificamente para o app 'catalog' ---"
-python manage.py migrate catalog --fake-initial
-
-# Passo 3: Cria o superusuário.
+python manage.py migrate
 python manage.py create_superuser_from_env
-
-echo "--- BUILD SCRIPT DE SINCRONIZACAO CONCLUIDO ---"
