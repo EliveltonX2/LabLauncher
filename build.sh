65a4c0d5613e8465f0c9098f9a0f9300a499ef60
EliveltonX2 <elivelton.pardini@gmail.com>
2025-06-22 02:13:28 -0400

    Prepara codigo final para deploy com banco de dados novo

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic
 python manage.py migrate
 python manage.py create_superuser_from_env
